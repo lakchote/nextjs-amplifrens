a7dc19f369ed5218b96a216195f47341ab6c085d
Lucien Akchoté <lakchote@icloud.com>
2022-10-27 23:24:20 +0200

    Improve spacing for lg screens

--- pages/profile/create.tsx
+++ pages/profile/create.tsx
@@ -2,7 +2,7 @@ import CreateProfileFormParent from "../../components/profile/CreateProfileFormP
 
 export default function CreateProfile() {
   return (
-    <main className="container mt-8">
+    <main className="container mt-8 lg:mt-10">
       <div className="text-center">
         <h2 className="text-2xl text-accent p-2 m-1">Create a profile (in less than 2mins)</h2>
         <p className="mt-2">
