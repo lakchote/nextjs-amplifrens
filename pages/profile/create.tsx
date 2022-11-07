872020ae8770b5a011dee3e20ba05682b206a557
Lucien Akchoté <lakchote@icloud.com>
2022-11-07 10:53:51 +0100

    Upd profile creation design

--- pages/profile/create.tsx
+++ pages/profile/create.tsx
@@ -3,19 +3,12 @@ import CreateProfileFormParent from "../../components/profile/CreateProfileFormP
 
 const CreateProfile: NextPage = () => {
   return (
-    <main className="container mt-8 lg:mt-10">
+    <main className="container p-10 bg-cover">
       <div className="text-center">
-        <h2 className="text-2xl text-primary p-2 m-1">Create a profile (in less than 2mins)</h2>
-        <p className="mt-2">
-          Creating a profile enables you to{" "}
-          <span className="text-secondary">
-            <strong>amplify your network</strong>
-          </span>
-          and
-          <span className="text-secondary">
-            <strong>make frens</strong>
-          </span>
-          .
+        <h2 className="text-2xl text-neutral p-2 m-1">Create a profile</h2>
+        <p className="mt-2 text-neutral">
+          Creating a profile enables you to <strong>amplify your network </strong>
+          and <strong>make frens</strong>.
         </p>
       </div>
       <CreateProfileFormParent />
